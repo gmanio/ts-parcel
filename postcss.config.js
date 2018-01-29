8d9c39e776171e7dd94ec260f6c0569657659845
JimanPark <jiman.park@sk.com>
2018-01-29 20:36:53 +0900

    [TASK] npm update

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,11 @@ module.exports = {
   plugins: {
     autoprefixer: {
       grid: true
+    },
+    'postcss-easysprites': {
+      imagePath: '../../../public/img',
+      spritePath: './public/sprites',
+      stylesheetPath: './src/assets/sprites/spr-img.css'
     }
   }
 };
